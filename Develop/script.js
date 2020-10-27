4297d90e07fb604c5fe968e8808f526f29fe0631
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-27 15:34:10 -0600

    Incorported both for loops into a single for loop

--- Develop/script.js
+++ Develop/script.js
@@ -2,26 +2,22 @@
 var m = moment();
 var hour = m.get('hour');
 var dateTime = m.format("LLLL");
-
 // Display the current day on top of the page
 $("#currentDay").text(dateTime);
 
-// listener for the save button
 $(document).ready(() => {
+    // listener for the save button
     $(".saveBtn").on("click", function () {
         // put task and time into variables and save them to local storage
         var userTask = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, userTask);
     });
-    // loop through the local storage for any saved tasks and populate them on the page
-    for (let i = 9; i <= 17; i++) {
-        $("#" + i + "-hour .description").val(localStorage.getItem(i + "-hour"))
-    }
-    // updated the classes based on the current time
-    function trackTime() {
+    function renderTasks() {
         // loop through every possible hour (9 - 17)
         for (let i = 9; i <= 17; i++) {
+            // loop through the local storage for any saved tasks and populate them on the page
+            $("#" + i + "-hour .description").val(localStorage.getItem(i + "-hour"))
             // pull every text area
             let timeBlock = document.getElementById(i);
             // if the text area id is equal to the current hour change class
@@ -36,5 +32,5 @@ $(document).ready(() => {
             }
         }
     }
-    trackTime();
+    renderTasks();
 });
